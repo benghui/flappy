3d66a7e0020a9ba2e95c30afaa53c53fba3d8de7
benghui <benghui@gmail.com>
2018-09-16 14:06:44 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//Written by Beng Hui
+
+
+var rows = 3;
+var columns = 3;
+
+
+var arr = [];
+for(var i=0; i<rows; i++) {
+    arr[i] = [];
+    for(var j=0; j<columns; j++) {
+        arr[i][j] = '.';
+    }
+}
